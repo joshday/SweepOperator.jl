5ad025a07aaa274aabbde7a9fe8d232630c671ff
Josh Day <emailjoshday@gmail.com>
2016-08-14 09:13:56 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,35 @@
 using SweepOperator
 using Base.Test
 
-x = randn(100, 10)
+n, p = 1000, 10
+x = randn(n, p)
 xtx = x'x
 
-A = deepcopy(xtx)
-B = deepcopy(xtx)
 
-for j in 1:4
-    sweep!(A, j)
-    sweep!(A, j, true)
+
+@testset "Sweep one by one" begin
+    A = deepcopy(xtx)
+    B = deepcopy(xtx)
+    for j in 1:p
+        sweep!(A, j)
+        sweep!(A, j, true)
+    end
+    @test A ≈ B
 end
 
-@testset "Tests" begin
+@testset "Sweep all" begin
+    A = deepcopy(xtx)
+    B = deepcopy(xtx)
+    sweep!(A, 1:p)
+    sweep!(A, 1:p, true)
     @test A ≈ B
+end
+
+@testset "Regression" begin
+    x = randn(n, p)
+    y = x * collect(1.:p) + randn(n)
+    xy = [x y]
+    xytxy = xy'xy
+    sweep!(xytxy, 1:p)
+    @test xytxy[1:p, end] ≈ x\y
 end
