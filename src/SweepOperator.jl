f460e7d7187f9a1027ce342af2ec50de1ab389c2
Josh Day <emailjoshday@gmail.com>
2017-06-23 10:42:33 -0700

    add sweep_with_buffer

--- src/SweepOperator.jl
+++ src/SweepOperator.jl
@@ -26,10 +26,37 @@ sweep!(xtx, 1, true)
 ```
 """
 function sweep!{T<:LinAlg.BlasFloat}(A::AMat{T}, k::Integer, inv::Bool = false)
+    # # ensure @inbounds is safe
+    # p = LinAlg.checksquare(A)
+    # d = one(T) / A[k, k]  # pivot
+    # akk = Symmetric(A)[:, k]  # k-th column (use Symmetric because only triu available)
+    # BLAS.syrk!('U', 'N', -d, akk, one(T), A)  # everything not in col/row k
+    # scale!(akk, d * (-one(T)) ^ inv)
+    # for i in 1:(k-1)  # col k
+    #     @inbounds A[i, k] = akk[i]
+    # end
+    # for j in (k+1):p  # row k
+    #     @inbounds A[k, j] = akk[j]
+    # end
+    # A[k, k] = -d  # pivot element
+    # A
+    sweep_with_buffer!(zeros(T, size(A, 2)), A, k, inv)
+end
+
+
+function sweep_with_buffer!{T<:LinAlg.BlasFloat}(akk::AVec{T}, A::AMat{T}, k::Integer,
+        inv::Bool = false)
     # ensure @inbounds is safe
     p = LinAlg.checksquare(A)
+    p == length(akk) || throw(DimensionError("buffer size incorrect"))
     d = one(T) / A[k, k]  # pivot
-    akk = Symmetric(A)[:, k]  # k-th column (use Symmetric because only triu available)
+    # get column A[:, k] (hack because only upper triangle is available)
+    for j in 1:k
+        @inbounds akk[j] = A[j, k]
+    end
+    for j in k+1:p
+        @inbounds akk[j] = A[k, j]
+    end
     BLAS.syrk!('U', 'N', -d, akk, one(T), A)  # everything not in col/row k
     scale!(akk, d * (-one(T)) ^ inv)
     for i in 1:(k-1)  # col k
@@ -43,8 +70,16 @@ function sweep!{T<:LinAlg.BlasFloat}(A::AMat{T}, k::Integer, inv::Bool = false)
 end
 
 function sweep!{T<:LinAlg.BlasFloat, I<:Integer}(A::AMat{T}, ks::AVec{I}, inv::Bool = false)
+    akk = zeros(T, size(A, 1))
     for k in ks
-        sweep!(A, k, inv)
+        sweep_with_buffer!(akk, A, k, inv)
+    end
+    A
+end
+function sweep_with_buffer!{T<:LinAlg.BlasFloat, I<:Integer}(akk::AVec{T}, A::AMat{T},
+        ks::AVec{I}, inv::Bool = false)
+    for k in ks
+        sweep_with_buffer!(akk, A, k, inv)
     end
     A
 end
